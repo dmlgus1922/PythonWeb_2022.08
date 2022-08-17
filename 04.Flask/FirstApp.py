bbe47383a6201abc4645cef5b0916f405d053c5f
dmlgus1922 <dmlgus1922@naver.com>
2022-08-17 15:09:12 +0900

    0817

--- 04.Flask/FirstApp.py
+++ 04.Flask/FirstApp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from pickle import GET
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template('01.index.html')
 
-@app.route('/menu')
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():
-    return render_template('02.menu.html')
-
+    if request.method == 'GET':
+        return render_template('02.menu.html')
+    else:
+        return render_template('03.menu_res.html')
+        
 if __name__ == '__main__':
     app.run(debug=True)
--- 04.Flask/SimpleApp.py
+++ 04.Flask/SimpleApp.py
@@ -6,7 +6,7 @@ def hello():
     return f'''
         <h1>Hello World!</h1>
         <hr>
-        <p>디버그 모드에서는 변경 사항이 발생하면 자동적으로 서버가 재실행됩니다.</p>
+        <p>변경 사항이 발생하면 자동적으로 서버가 재실행됩니다.</p>
 '''
 if __name__ == '__main__':
     app.run(debug=True)
